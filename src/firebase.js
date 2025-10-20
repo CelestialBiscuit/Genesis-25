8caa0ce74da6a74da78206dadd4c074f5e7b7ee8
Jasleen1210 <05kaur.j@gmail.com>
2025-10-20 10:33:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyA1-x24mDIebpvRC6FthbWvK1lXMaEPLUg",
+  authDomain: "crossword--25.firebaseapp.com",
+  projectId: "crossword--25",
+  storageBucket: "crossword--25.firebasestorage.app",
+  messagingSenderId: "1047114940255",
+  appId: "1:1047114940255:web:7264fce491405d178c40ea",
+  measurementId: "G-TLSWNESD2Z"
 };
 
 
