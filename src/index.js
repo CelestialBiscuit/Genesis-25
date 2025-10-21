3bf4e32b9873cf9f572e75aa6b9c5bc844844782
Jasleen1210 <05kaur.j@gmail.com>
2025-10-21 19:07:00 +0530

    vercel web analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 import "@fontsource/poppins/100.css"; // Thin
 import "@fontsource/poppins/200.css"; // ExtraLight
@@ -18,6 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
